53c1c2471c5be62d8731c3b0f33d343ac2af707f
JonasUJ <jj@ifs.aero>
2025-05-23 13:28:32 +0200

    More input validation

--- examgaming/input_validators/input_validator.py
+++ examgaming/input_validators/input_validator.py
@@ -1,5 +1,6 @@
 import sys
 import string
+from collections import Counter
 
 try:
     alpha = set(string.ascii_lowercase)
@@ -15,6 +16,8 @@ try:
 
     assert len(lines) == n
 
+    problems = []
+
     for line in lines:
         assert line != "\n"
         assert not line.startswith(" ")
@@ -22,11 +25,19 @@ try:
         assert not line.endswith("\r\n")
         
         K, M, P, T = line.split(" ")
+        problems.append((K, M, P, T))
 
         assert alpha.issuperset(K)
         assert alpha.issuperset(M)
         assert 1 <= int(P) <= 200
         assert 1 <= int(T) <= 100
+
+    name_counter = Counter([K for K, *_ in problems])
+    method_counter = Counter([(K, M) for K, M, *_ in problems])
+
+    assert name_counter.most_common(1)[0][1] <= 3 # Only up to three problems with same name
+    assert method_counter.most_common(1)[0][1] == 1 # At most one way to solve a problem
+    
 except:
     exit(43)
 
